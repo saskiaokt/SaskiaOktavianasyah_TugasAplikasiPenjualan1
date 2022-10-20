e0fce87ca3cc69b3544d5c09cf9464b8948c8637
saskia <saskiaoktavianasyah@gmail.com>
2022-10-20 17:30:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasaplikasipenjualan/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.tugasaplikasipenjualan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
